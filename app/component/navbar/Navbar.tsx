1e59574cd247520d1037c98ddf2789560b007613
Eyre Xi <109657355+Eyre-dutchess@users.noreply.github.com>
2025-06-22 09:28:28 +0800

    Update Navbar.tsx

--- app/component/navbar/Navbar.tsx
+++ app/component/navbar/Navbar.tsx
@@ -39,7 +39,7 @@ export const Navbar = () => {
   }, [])
 
   return(
-    <div className={`w-screen fixed top-0 left-0 z-50 ${scrolled ? "bg-white drop-shadow-blue drop-shadow-md":"bg-transparent drop-shadow-none "}`} >
+    <div className={`w-screen fixed top-0 left-0 z-40 ${scrolled ? "bg-white drop-shadow-blue drop-shadow-md":"bg-transparent drop-shadow-none "}`} >
       <Container>
           <div className='w-full py-2 flex flex-row items-center justify-between relative z-50'>
             <div className='cursor-pointer w-[2em] h-[2em] ml-2 rounded-full overflow-hidden relative' onClick={()=> router.push("/")}>
